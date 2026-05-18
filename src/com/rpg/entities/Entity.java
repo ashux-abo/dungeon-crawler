b0661942b30e4c7d47b6ed04eedb620755f451f4
ashux-abo <dulayashley12@gmail.com>
2026-05-19 07:31:45 +0800

    Initialize the Entity and create a take damage method

---
+++ src/com/rpg/entities/Entity.java
@@ -0,0 +1,23 @@
+package com.rpg.entities;
+
+public abstract class Entity {
+  protected String name;
+  protected int hp;
+  protected int maxHp;
+  protected int attackPower;
+
+  public Entity(String name, int hp, int maxHp, int attackPower) {
+    this.name = name;
+    this.hp = hp;
+    this.maxHp = maxHp;
+    this.attackPower = attackPower;
+  }
+
+  public void takeDamage(int damage) {
+    this.hp -= damage;
+    if (this.hp < 0)
+      this.hp = 0;
+    System.out.println(name + " takes " + damage + " damge! (HP: " + hp + "/" + maxHp + ")");
+
+  }
+}
